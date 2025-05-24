2008a5ab4b7d8b64c19d528f65dc867a391be63f
zseer001 <zwjwyzq@gmail.com>
2025-05-24 13:54:11 +0800

    replace with astrofy

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,10 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
+import astrofy from 'astrofy';
 
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://example.com',
-	integrations: [mdx(), sitemap()],
+	integrations: [mdx(), sitemap(), astrofy()],
 });
